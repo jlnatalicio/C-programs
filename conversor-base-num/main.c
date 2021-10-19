e0cb4af2786cf103cf4b656a87582a883c1191c1
José Leonardo <33907953+jlnatalicio@users.noreply.github.com>
2021-10-19 19:30:14 -0300

    Update - versão 2.0

--- conversor-base-num/main.c
+++ conversor-base-num/main.c
@@ -1,37 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
+      int opcao, valor;
+      int program_is_running = 1;
+      
+      printf("Conversor de Bases Numericas v2.0\n\n");
+      fflush(stdout);
+      
+      while (program_is_running)
+      {
+            printf("Selecione uma das opcoes abaixo:\n");
+            fflush(stdout);
+            printf("1: decimal para hexadecimal\n");
+            fflush(stdout);
+            printf("2: hexadecimal para decimal\n");
+            fflush(stdout);
+            printf("3: fechar programa\n");
+            fflush(stdout);
+            printf("Informe a opcao: ");
+            fflush(stdout);
+            scanf(" %d", &opcao);
+            
+            if (opcao == 1)
+            {
+                  printf("Informe o valor decimal: ");
+                  fflush(stdout);
+                  scanf(" %d", &valor);
+                  
+                  printf("%d em decimal e igual a %x em hexadecimal!\n\n", valor, valor);
+                  fflush(stdout);
+            }
+            else if (opcao == 2)
+            {
+                  printf("Informe o valor hexadecimal: ");
+                  fflush(stdout);
+                  scanf(" %x", &valor);
+                  
+                  printf("%x em hexadecimal e igual a %d em decimal!\n\n", valor, valor);
+                  fflush(stdout);
+            }
+            else if (opcao == 3)
+            {
+                  printf("Finalizando programa...\n");
+                  fflush(stdout);
+                  program_is_running = 0;
+            }
+            else
+            {
+                  printf("Opcao invalida.\n\n");
+                  fflush(stdout);
+            }
+      }
 	
-	int opcao, valor;
-	
-	printf("Conversor de Bases Numericas v1.0\n\n");
-	
-	printf("1: decimal para hexadecimal\n");
-	printf("2: hexadecimal para decimal\n");
-	printf("Informe a opcao: ");
-	scanf("%d", &opcao);
-	fflush(stdin);
-	
-	if (opcao == 1)
-	{
-		printf("Informe o valor decimal: ");
-		scanf("%d", &valor);
-		fflush(stdin);
-		
-		printf("%d em decimal e igual a %x em hexadecimal!\n", valor, valor);
-	}
-	else if (opcao == 2)
-	{
-		printf("Informe o valor hexadecimal: ");
-		scanf("%d", &valor);
-		fflush(stdin);
-		
-		printf("%x em hexadecimal e igual a %d em decimal!\n", valor, valor);
-	}
-	else printf("Opcao invalida.\n");
-	
-	printf("\nPressione uma tecla para continuar...");
-	getchar();
-	return 0;
+      system("PAUSE");
+      return 0;
 }
